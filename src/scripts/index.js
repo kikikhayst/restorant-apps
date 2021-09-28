0b3c389dc8fbec74333f6c7eace7bd854891eb3b
RIZQI RAMA HAYUSTYA <73214485+kikikhayst@users.noreply.github.com>
2021-09-28 21:23:30 +0700

    Add files via upload

---
+++ src/scripts/index.js
@@ -0,0 +1,44 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/responsive.css';
+
+console.log('Hello Coders! :)');
+import (`../DATA.json`).then(({default: DATAjson}) => {
+    const content = DATAjson.restaurants;
+    let listContent = ``;
+    content.forEach(function (data) {
+        listContent += `
+        <div class="item">
+            <div>
+                <img class="itemPicture" src="${data.pictureId}" alt="${data.name}" title="${data.name}">
+            </div>
+            <p class="itemCity">${data.city}</p>
+            <div class="itemDesc">
+                <h1><a href="#">${data.name}</a></h1>
+                <div>
+                    <p>${data.description.slice(0, 150)}...</p>
+                </div>
+                <p class="itemRating">Rating: <a href="#">${data.rating}</a></p>
+            </div>
+        </div>`
+    })
+    document.querySelector('#mainItem').innerHTML = listContent;
+})
+
+const menu = document.querySelector('#headerBurger');
+const hero = document.querySelector('.jumbotron');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function (event) {
+    drawer.classList.toggle('open');
+    event.stopPropagation();
+});
+
+hero.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
+
+main.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
